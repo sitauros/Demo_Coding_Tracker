77d75c850f67fdb510f441855f897ce2005a40fd
sitauros <sitauros@gmail.com>
2023-03-14 15:13:18 -0400

    Added sample list for empty database scenario

--- Coding Tracker/Controller.cs
+++ Coding Tracker/Controller.cs
@@ -16,7 +16,29 @@ namespace CodingTracker
         static void Main(string[] args)
         {
             Controller controller = new Controller(ConfigurationManager.AppSettings.Get("ConnectionString"), ConfigurationManager.AppSettings.Get("TableName"));
+           
+            if (controller.Model.GetRecordCount() == 0)
+            {
+                generateSessionList(controller.Model);  
+            }
+
             controller.View.PrintMainMenu();
+        }
+
+        internal static void generateSessionList(Model model)
+        {
+            Random randomizer = new Random();
+            for (int i = 1; i <= 20; i++)
+            {
+                int offset_days = randomizer.Next(0, 365);
+                int offset_hours = randomizer.Next(0, 24);
+                int offset_minutes = randomizer.Next(0, 60);
+
+                DateTime StartTime = DateTime.Now;
+                DateTime EndTime = StartTime.Add(new TimeSpan(offset_days, offset_hours, offset_minutes, 0));
+                string duration = Validation.CalculateDuration(StartTime, EndTime);
+                model.AddSession(new CodingSession(i, StartTime.ToString("g"), EndTime.ToString("g"), duration));
+            }
         }
 
         internal static void ExitProgram()
